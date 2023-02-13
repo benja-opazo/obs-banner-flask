9ee81c98876a5973e7d3c227eff0b9657b748c7a
Benja O <benjamin.opazo.c@gmail.com>
2023-02-13 00:56:48 -0300

    updated run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,14 @@
+# Create Venv if it does not exist
+If(!(test-path -PathType container .venv))
+{
+      python -m venv .venv
+}
+
 # Activate venv
 .venv\Scripts\activate.ps1
+
 # Install requirements
 pip install -r requirements.txt
+
+# Run Flask
 flask run --host=0.0.0.0
